f908ad301d683b351a7bd03fefa9de6591796c24
karla-walker <indiablonix@gmail.com>
2021-07-11 22:28:03 -0700

    Inital Nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,17 +2,19 @@ import React from 'react';
 
 const Nav = () => {
   return (
-    <ul>
-      <li>
-        <a href="#">Cats</a>
-      </li>
-      <li>
-        <a href="#">Dogs</a>
-      </li>
-      <li>
-        <a href="#">Computers</a>
-      </li>
-    </ul>
+    <nav className="main-nav">
+      <ul>
+        <li>
+          <a href="#">Cats</a>
+        </li>
+        <li>
+          <a href="#">Dogs</a>
+        </li>
+        <li>
+          <a href="#">Computers</a>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
