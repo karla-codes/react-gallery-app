d77c5805187febb92848fab6660c4b92a2c06cbe
karla-walker <indiablonix@gmail.com>
2021-07-12 02:11:39 -0700

    Deleted comments

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,6 @@ import Nav from './Nav';
 
 function App() {
   return <Nav />;
-  {
-    /* Search form - stateful component */
-  }
-  {
-    /* <Form /> */
-  }
-
-  {
-    /* Navigation - stateless component */
-  }
-
-  {
-    /* Photo container - stateful component */
-  }
-  {
-    /* <PhotoContainer /> */
-  }
 }
 
 export default App;
