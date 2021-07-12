6170e458c517fa99db1dda9606f327655f304e20
karla-walker <indiablonix@gmail.com>
2021-07-11 22:22:57 -0700

    Reorganized files into folders and removed unnecessary files

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import '../css/index.css';
+import Nav from './Nav';
+
+function App() {
+  return <Nav />;
+  {
+    /* Search form - stateful component */
+  }
+  {
+    /* <Form /> */
+  }
+
+  {
+    /* Navigation - stateless component */
+  }
+
+  {
+    /* Photo container - stateful component */
+  }
+  {
+    /* <PhotoContainer /> */
+  }
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <ul>
+      <li>
+        <a href="#">Cats</a>
+      </li>
+      <li>
+        <a href="#">Dogs</a>
+      </li>
+      <li>
+        <a href="#">Computers</a>
+      </li>
+    </ul>
+  );
+};
+
+export default Nav;
