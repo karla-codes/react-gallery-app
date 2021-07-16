f4c821a3f2acf6aa3ecb44d4b67fd634a910d9a3
karla-walker <indiablonix@gmail.com>
2021-07-15 20:23:14 -0700

    Created a 404-like error component

---
+++ src/components/Error404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Error404 = () => {
+  return (
+    <li className="not-found">
+      <h3>404</h3>
+      <p>Uh oh! This page does not exist.</p>
+    </li>
+  );
+};
+
+export default Error404;
